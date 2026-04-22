8bd9917b3d6d95c3ae97bad8b150d2c60b008e6f
Stefan Fiedler <stefan.fiedler@spotteron.net>
2026-04-22 21:17:41 +0200

    03-optimizer

--- src/all-steps/main.py
+++ src/all-steps/main.py
@@ -1,11 +1,14 @@
+import argparse
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
 import lightning as L
 
 
 class Net(L.LightningModule):
-    def __init__(self):
+    def __init__(self, args):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, 3, 1)
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
@@ -13,6 +16,8 @@ class Net(L.LightningModule):
         self.dropout2 = nn.Dropout(0.5)
         self.fc1 = nn.Linear(9216, 128)
         self.fc2 = nn.Linear(128, 10)
+
+        self.args = args
 
     def forward(self, x):
         x = self.conv1(x)
@@ -35,9 +40,22 @@ class Net(L.LightningModule):
         loss = F.nll_loss(output, target)
         return {'loss': loss}
 
+    def configure_optimizers(self):
+        optimizer = optim.Adadelta(self.parameters(), lr=self.args.lr)
+        scheduler = StepLR(optimizer, step_size=1, gamma=self.args.gamma)
+        return {'optimizer': optimizer, 'lr_scheduler': scheduler}
+
 
 def main():
-    model = Net()
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch Lightning MNIST Example')
+    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
+                        help='learning rate (default: 1.0)')
+    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
+                        help='Learning rate step gamma (default: 0.7)')
+    args = parser.parse_args()
+
+    model = Net(args)
 
     trainer = L.Trainer()
     trainer.fit(model=model)
